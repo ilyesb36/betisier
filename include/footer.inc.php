19a4179b9cea098fa88f9ade4bb7329b4afd4673
Ruizheng <ruizhengxu@gmail.com>
2019-12-22 16:38:34 +0100

    Ajout copyright

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -2,7 +2,9 @@
 <div id="footer">
 Le bétisier de l'IUT à votre service, depuis novembre 2018
 <br />
-    © IUT du Limousin  DUT Informatique année 2 
+    © IUT du Limousin  DUT Informatique année 2
+<br />
+    © XU Ruizheng
 
 </div>
 </body>
